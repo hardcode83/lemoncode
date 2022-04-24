1cdb36e31c2c939d14a5450b4c844adeaac678de
hardcode83 <josegascon@gmail.com>
2022-04-24 21:08:58 +0200

    check

---
+++ Modulo-1-Linux/4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+URL=$1
+FILE=$2
+WORD=$3
+if [[ $# < 3 ]]; then
+ echo "USAGE: ./$0 URL FILETOSAVE WORDTOFIND"
+ echo "Example: ./$0 https://www.google.com google.com.txt var"
+ exit 2
+else
+ curl -s $URL -o $FILE
+ grep -n $WORD $FILE |  cut -f1 -d:
+fi
