5110ff0a622f9bd187780734f012294a4c84f1af
hardcode83 <josegascon@gmail.com>
2022-04-24 22:10:09 +0200

    moving aux files

--- Modulo-2-Contenedores-Docker_laboratorio/lemoncode-challenge/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-TEST=$(curl -s localhost:8080 | grep -i "Contenedores" | sed -e 's/^[[:space:]]*//i')
-if [["$TEST" == "Contenedores"]]; then
-       	echo "Success!"
-else
-	echo "Any problem with deploy"
-fi
